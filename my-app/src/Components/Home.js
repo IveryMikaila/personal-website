c82c6764d322f853d01979b3280e595ca99a2411
IveryMikaila <112774755+IveryMikaila@users.noreply.github.com>
2023-07-08 17:08:53 -0400

    Added Resume Pdf Asset, Styled Home page

--- my-app/src/Components/Home.js
+++ my-app/src/Components/Home.js
@@ -1,11 +1,28 @@
 import '../Styles/Home.css'
 import ScrollAnimation from './SectionWipes';
+import Navigation from './Sidebar'
+import resume from '../assets/MIvery_Resume.pdf'
 
 const Home = () => {
   return (
     <>
 <div className='home-container'>
-<div className='home-section'>Home</div>
+<div className='home-section'>
+  <div className='home-content'>
+  <Navigation /> 
+  <div className='home-intro'>
+<h1 className='home-title'>Hello. I'm a freelance frontend developer</h1>
+<p>My name is Mikaila Ivery. I code webpages using modern frontend web technologies and my peculiar mind.</p>
+
+<a href='#contact'>Let's work together!</a>
+  </div>
+  <div className='home-footer'>
+    Check me out on <a href='https://www.linkedin.com/in/mikaila-ivery-259218223/' target='_blank'>Linkedin</a>, <a href='https://www.youtube.com/watch?v=hMif-EdLGAs&t=20s&ab_channel=MikailaIvery' target='_blank'>Youtube</a>, and <a href='https://github.com/IveryMikaila' target='_blank'>GitHub.</a>
+    
+    <p>Download <a href={resume} download="Resume">my resume </a>(PDF 85kb)</p>
+  </div>
+  </div>
+</div>
 <ScrollAnimation />
 </div>
     </>
--- my-app/src/Components/SectionWipes.js
+++ my-app/src/Components/SectionWipes.js
@@ -1,39 +1,10 @@
 import React from 'react';
-import styled from 'styled-components';
+import '../Styles/Home.css'
 import { Controller, Scene } from 'react-scrollmagic';
 
-const SectionWipesStyled = styled.div`
-width: 50%;
-margin-left:50%;
-
-  .panel {
-    height: 100vh;
-  }
-    
-  .panel.one {
-    background-color: #3883d8;
-  }
-  
-  .panel.two {
-    background-color: #38ced7;
-  }
-  
-  .panel.three {
-    background-color: #22d659;
-    margin-bottom: 800px;
-  }
-  
-  .panel.four {
-    background-color: #953543;
-  }
-  .panel.five {
-    background-color: #f62680;
-  }
-
-`;
 
 const SectionWipes = () => (
-  <SectionWipesStyled>
+  <div className='animation-container'>
     <Controller globalSceneOptions={{ triggerHook: 'onLeave' }}>
       <Scene pin>
         <div className="panel one">
@@ -62,7 +33,7 @@ const SectionWipes = () => (
         </div>
       </Scene>
     </Controller>
-  </SectionWipesStyled>
+  </div>
 );
 
 export default SectionWipes;
