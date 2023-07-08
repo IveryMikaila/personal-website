bb9e96442e81eef5e47644bd098c039969807c6d
IveryMikaila <112774755+IveryMikaila@users.noreply.github.com>
2023-07-08 13:51:05 -0400

    Created Component Files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,10 @@
+import React from "react";
+import Home from './Components/Home'
+
 function App() {
   return (
     <div className="App">
-
+<Home />
     </div>
   );
 }
---
+++ my-app/src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+//Component Imports
+
+
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home;
