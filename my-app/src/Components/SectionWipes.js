d911ea8c907c35f7920fa6314326ba5ca427dc55
IveryMikaila <112774755+IveryMikaila@users.noreply.github.com>
2023-07-08 18:17:47 -0400

    Added a new font asset & image asset

--- my-app/src/Components/SectionWipes.js
+++ my-app/src/Components/SectionWipes.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import '../Styles/Home.css'
 import { Controller, Scene } from 'react-scrollmagic';
-
+import video from '../assets/video.mp4';
+import image from '../assets/brain.gif'
 
 const SectionWipes = () => (
   <div className='animation-container'>
     <Controller globalSceneOptions={{ triggerHook: 'onLeave' }}>
       <Scene pin>
         <div className="panel one">
-<div>VIDEO</div>
-
+        {/* <video controls autoPlay loop width="100%" height="100%">
+    <source src={video} type="video/mp4" />
+</video> */}
+<img width="100%" height="100%" src={image} alt='brain x-ray'/>
         </div>
       </Scene>
       <Scene pin>
