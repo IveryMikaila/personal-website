c3f5cb6d4d33445e483f3bf9fa11181159e87e5e
IveryMikaila <112774755+IveryMikaila@users.noreply.github.com>
2023-07-10 15:40:16 -0400

    Added Branding styles

--- my-app/src/Components/Page Sections/Brands.js
+++ my-app/src/Components/Page Sections/Brands.js
@@ -4,33 +4,36 @@ import '../../Styles/Other.css'
 const Brands = () => {
   return (
    <>
-   <div className='brand-wrappers'>
+   <div className='brand-wrapper'>
     {/* Brand 1 */}
    <div className='brand-card oree'>
       <h1 className='brand-oree'>Orée Nyc</h1>
       <div className='brand-content'>
-      Lorem ipsum dolor sit amet. Non voluptates placeat ab sunt soluta ab fugiat culpa vel autem corrupti 
-      </div>
+        <h3>Orée Nyc</h3>
+Hired to design and code mock designs and wireframes for landing pages. Technologies used include Figma, HTML5, CSS3, Bootstrap, and JavaScript.
+</div>
     </div>
        {/* Brand 2 */}
    <div className='brand-card bb'>
       <h1 className='brand-bb'>Boy Blondey</h1>
       <div className='brand-content'>
-      Lorem ipsum dolor sit amet. Non voluptates placeat ab sunt soluta ab fugiat culpa vel autem corrupti 
-      </div>
+      <h3>Boy Blondey</h3>
+      Hired to customize/code Shopify liquid theme template and code HTML Emails. Technologies used include HTML5, CSS3, JavaScript, and React.js.</div>
     </div>
-       {/* Brand 3 */}
+     {/* Brand 3 */}
    <div className='brand-card hair'>
       <h1 className='brand-hair'>Brooklyn House<br/>of Hair</h1>
       <div className='brand-content'>
-      Lorem ipsum dolor sit amet. Non voluptates placeat ab sunt soluta ab fugiat culpa vel autem corrupti 
+      <h3>Brooklyn House of Hair</h3>
+      Hired to customize/code Shopify liquid theme template and design hero section. Technologies used include Figma, HTML5, CSS3, and JavaScript.
       </div>
     </div>
        {/* Brand 4 */}
    <div className='brand-card moojy'>
    <h1 className='brand-moojy'>Waist Away <span className='moojy-font2'>by Moojy</span></h1>
       <div className='brand-content'>
-      Lorem ipsum dolor sit amet. Non voluptates placeat ab sunt soluta ab fugiat culpa vel autem corrupti 
+      <h3>Waist Away by Moojy</h3>
+      Hired to customize/code Shopify liquid theme template and design hero section. Technologies used include Figma, HTML5, CSS3, and JavaScript.
       </div>
     </div>
 
