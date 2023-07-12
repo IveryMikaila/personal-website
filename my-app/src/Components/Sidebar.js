992dbc017f1511b8dc95bfd018b0f98516b9d147
IveryMikaila <112774755+IveryMikaila@users.noreply.github.com>
2023-07-12 01:07:17 -0400

    Fixed nav icon bug

--- my-app/src/Components/Sidebar.js
+++ my-app/src/Components/Sidebar.js
@@ -9,9 +9,8 @@ const showMenu = () => setMenu(!menu);
   return (
     <div className='nav-container'>
       {/* Nav Button */}
-      <div className='icon-wrapper'>
       <svg className='nav-icon' onClick={showMenu} stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><g><path fill="none" d="M0 0h24v24H0z"></path><path d="M3 4h18v2H3V4zm0 7h12v2H3v-2zm0 7h18v2H3v-2z"></path></g></svg>
-      </div>
+   
     
        {/* Navigation */}
       <nav className={menu ? 'nav-wrapper active' : 'nav-wrapper'}>
