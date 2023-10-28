b1f47327c9eb862551cdd94d248f2472ab7a9252
IveryMikaila <112774755+IveryMikaila@users.noreply.github.com>
2023-10-28 02:10:59 -0400

    Fixed Contact Form Bugs
    -Removed Email JS API
    -Redesigned Contact Form

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './Components/Home'
+import Thanks from "./Components/Thanks";
+
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
       <Home />
-      </BrowserRouter>
+
+<Routes>
+<Route path="/thanks" element={<Thanks />} />
+</Routes>
+  </BrowserRouter>
     </div>
   );
 }
